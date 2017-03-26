f2910160d1f5c35e261e5de8af3883acb3b1fad2
Jak_o_Shadows <simfake.mapping@gmail.com>
2017-03-26 12:24:43 +1030

    Moved USB into it's own subfile. Added simple output test function

---
+++ code/lowlevel/usb.c
@@ -0,0 +1,381 @@
+/*
+ * usb.c
+ * 
+ * Copyright 2017 jak <jak@linux-quu2>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include "usb.h"
+
+
+uint8_t usbd_control_buffer[128];
+
+
+
+
+
+static const uint8_t hid_report_descriptor[] = {
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+    0x09, 0x04,                    // USAGE (Joystick)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x05, 0x01,                    //   USAGE_PAGE (Generic Desktop)
+    0xa1, 0x00,                    //   COLLECTION (Physical)
+    0x09, 0x30,                    //     USAGE (X)
+    0x09, 0x31,                    //     USAGE (Y)
+    0x09, 0x32,                    //     USAGE 
+    0x09, 0x33,                    //     USAGE 
+    0x09, 0x34,
+    0x09, 0x35,
+	0x15, 0x00,        //     Logical Minimum (0)
+	0x26, 0xFF, 0x00,  //     Logical Maximum (255)
+	0x75, 0x08,        //     Report Size (8)
+    0x95, 0x06,                    //     REPORT_COUNT (6)					6 byte
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0xc0,                          //   END_COLLECTION
+    0x09, 0x39,                    //   USAGE (Hat switch)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x25, 0x07,                    //   LOGICAL_MAXIMUM (7)
+    0x35, 0x00,                    //   PHYSICAL_MINIMUM (0)
+    0x46, 0x0e, 0x01,              //   PHYSICAL_MAXIMUM (270)
+    0x65, 0x14,                    //   UNIT (Eng Rot:Angular Pos)
+    0x75, 0x04,                    //   REPORT_SIZE (4)
+    0x95, 0x01,                    //   REPORT_COUNT (1)					1/2 byte
+    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+    0x05, 0x09,                    //   USAGE_PAGE (Button)
+    0x19, 0x01,                    //   USAGE_MINIMUM (Button 1)
+    0x29, 0x04,                    //   USAGE_MAXIMUM (Button 4)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+    0x75, 0x01,                    //   REPORT_SIZE (1)
+    0x95, 0x04,                    //   REPORT_COUNT (4)					1/2 byte
+    0x55, 0x00,                    //   UNIT_EXPONENT (0)
+    0x65, 0x00,                    //   UNIT (None)
+    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+    0x05, 0x09,                    //   USAGE_PAGE (Button)   //add anothign lot of buttons 
+    0x19, 0x05,                    //   USAGE_MINIMUM (Button 5)
+    0x29, 0x14,                    //   USAGE_MAXIMUM (Button 20)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+    0x75, 0x01,                    //   REPORT_SIZE (1)
+    0x95, 0x10,                    //   REPORT_COUNT (16)					2 byte
+    0x55, 0x00,                    //   UNIT_EXPONENT (0)
+    0x65, 0x00,                    //   UNIT (None)
+    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+    0xc0                           // END_COLLECTION 
+};
+
+
+
+static const struct {
+	struct usb_hid_descriptor hid_descriptor;
+	struct {
+		uint8_t bReportDescriptorType;
+		uint16_t wDescriptorLength;
+	} __attribute__((packed)) hid_report;
+} __attribute__((packed)) hid_function = {
+	.hid_descriptor = {
+		.bLength = sizeof(hid_function),
+		.bDescriptorType = USB_DT_HID,
+		.bcdHID = 0x0100,
+		.bCountryCode = 0,
+		.bNumDescriptors = 1,
+	},
+	.hid_report = {
+		.bReportDescriptorType = USB_DT_REPORT,
+		.wDescriptorLength = sizeof(hid_report_descriptor),
+	}
+};
+
+const struct usb_endpoint_descriptor hid_endpoint = {
+	.bLength = USB_DT_ENDPOINT_SIZE,
+	.bDescriptorType = USB_DT_ENDPOINT,
+	.bEndpointAddress = 0x81,
+	.bmAttributes = USB_ENDPOINT_ATTR_INTERRUPT,
+	.wMaxPacketSize = 64,   //WAS 4 - CHANGED TO TEST SOMETHING ON THE 2-12-2016 BY J
+	.bInterval = 0x02,
+};
+
+const struct usb_interface_descriptor hid_iface = {
+	.bLength = USB_DT_INTERFACE_SIZE,
+	.bDescriptorType = USB_DT_INTERFACE,
+	.bInterfaceNumber = 0,
+	.bAlternateSetting = 0,
+	.bNumEndpoints = 1,
+	.bInterfaceClass = USB_CLASS_HID,
+	.bInterfaceSubClass = 1, /* boot */
+	.bInterfaceProtocol = 2, /* joystick */
+	.iInterface = 0,
+
+	.endpoint = &hid_endpoint,
+
+	.extra = &hid_function,
+	.extra_len = sizeof(hid_function),
+};
+
+#ifdef INCLUDE_DFU_INTERFACE
+const struct usb_dfu_descriptor dfu_function = {
+	.bLength = sizeof(struct usb_dfu_descriptor),
+	.bDescriptorType = DFU_FUNCTIONAL,
+	.bmAttributes = USB_DFU_CAN_DOWNLOAD | USB_DFU_WILL_DETACH,
+	.wDetachTimeout = 255,
+	.wTransferSize = 1024,
+	.bcdDFUVersion = 0x011A,
+};
+
+const struct usb_interface_descriptor dfu_iface = {
+	.bLength = USB_DT_INTERFACE_SIZE,
+	.bDescriptorType = USB_DT_INTERFACE,
+	.bInterfaceNumber = 1,
+	.bAlternateSetting = 0,
+	.bNumEndpoints = 0,
+	.bInterfaceClass = 0xFE,
+	.bInterfaceSubClass = 1,
+	.bInterfaceProtocol = 1,
+	.iInterface = 0,
+
+	.extra = &dfu_function,
+	.extra_len = sizeof(dfu_function),
+};
+#endif
+
+const struct usb_interface ifaces[] = {{
+	.num_altsetting = 1,
+	.altsetting = &hid_iface,
+#ifdef INCLUDE_DFU_INTERFACE
+}, {
+	.num_altsetting = 1,
+	.altsetting = &dfu_iface,
+#endif
+}};
+
+const struct usb_config_descriptor config[] = {{
+	.bLength = USB_DT_CONFIGURATION_SIZE,
+	.bDescriptorType = USB_DT_CONFIGURATION,
+	.wTotalLength = 0,
+#ifdef INCLUDE_DFU_INTERFACE
+	.bNumInterfaces = 2,
+#else
+	.bNumInterfaces = 1,
+#endif
+	.bConfigurationValue = 1,
+	.iConfiguration = 0,
+	.bmAttributes = 0xC0,
+	.bMaxPower = 0x32,
+
+	.interface = ifaces,
+}};
+
+const struct usb_device_descriptor dev_descr = {
+	.bLength = USB_DT_DEVICE_SIZE,
+	.bDescriptorType = USB_DT_DEVICE,
+	.bcdUSB = 0x0200,
+	.bDeviceClass = 0,
+	.bDeviceSubClass = 0,
+	.bDeviceProtocol = 0,
+	.bMaxPacketSize0 = 64,
+	.idVendor = 0x0483,
+	.idProduct = 0x5710,
+	.bcdDevice = 0x0200,
+	.iManufacturer = 1,
+	.iProduct = 2,
+	.iSerialNumber = 3,
+	.bNumConfigurations = 1,
+
+	.config = config
+};
+
+static const char *usb_strings_ascii[] = {
+	"Black Sphere Technologies",
+	"JOYSTICK",
+	"DEMO",
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+static enum usbd_control_result
+control_callback(usbd_device *dev, struct usbd_control_arg *arg)
+{
+	enum usbd_control_result result;
+
+	result = hid_control_request(dev, arg);
+
+#ifdef INCLUDE_DFU_INTERFACE
+	if (result == USBD_REQ_NEXT) {
+		result = dfu_control_request(dev, arg);
+	}
+#endif
+
+	return result;
+}
+
+int usb_strings(usbd_device *dev, struct usbd_get_string_arg *arg)
+{
+	(void)dev;
+	return usbd_handle_string_ascii(arg, usb_strings_ascii, 3);
+}
+
+int hid_control_request(usbd_device *dev, struct usbd_control_arg *arg)
+{
+	(void)dev;
+
+	if(	(arg->setup.bmRequestType != 0x81) ||
+		(arg->setup.bRequest != USB_REQ_GET_DESCRIPTOR) ||
+		(arg->setup.wValue != 0x2200)) {
+		return USBD_REQ_NEXT;
+	}
+
+	/* Handle the HID report descriptor. */
+	arg->buf = (uint8_t *)hid_report_descriptor;
+	if (arg->len > sizeof(hid_report_descriptor)) {
+		arg->len = sizeof(hid_report_descriptor);
+	}
+
+	return USBD_REQ_HANDLED;
+}
+
+
+
+void hid_set_config(usbd_device *dev,
+				const struct usb_config_descriptor *cfg)
+{
+	(void)cfg;
+
+	usbd_ep_setup(dev, 0x81, USB_ENDPOINT_ATTR_INTERRUPT, 4, NULL);
+
+#if defined(__ARM_ARCH_6M__)
+	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
+	/* SysTick interrupt every N clock pulses: set reload to N-1 */
+	systick_set_reload(99999*8);
+#else
+	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
+	/* SysTick interrupt every N clock pulses: set reload to N-1 */
+	systick_set_reload(99999);
+#endif
+	systick_interrupt_enable();
+	systick_counter_enable();
+}
+
+void __attribute__((weak))
+usbhid_target_usbd_after_init_and_before_first_poll(void) { /* empty */ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//USB is all tucked away now - therefore things like usbd_dev are not in
+//the namespace of the main file. Therefore need some helper functins.
+
+
+void usbSetup(void){
+	usbhid_target_init();
+
+	usbd_dev = usbd_init(usbhid_target_usb_driver(), &dev_descr,
+		usbd_control_buffer, sizeof(usbd_control_buffer));
+
+	usbd_register_set_config_callback(usbd_dev, hid_set_config);
+	usbd_register_control_callback(usbd_dev, control_callback);
+	usbd_register_get_string_callback(usbd_dev, usb_strings);
+	
+	
+}
+
+
+void usbInLoop(void){
+	usbd_poll(usbd_dev);
+}
+
+
+void writeToEndpoint(uint8_t endpoint, uint8_t buf[], uint16_t len){
+	usbd_ep_write_packet(usbd_dev, endpoint, buf, len);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
