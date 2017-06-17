9b7c1f2f565433f1f56cba53304b036d249857f3
Jak_o_Shadows <simfake.mapping@gmail.com>
2017-06-17 17:04:10 +0930

    analog mux - initial version

---
+++ code/lowlevel/analogMux.h
@@ -0,0 +1,42 @@
+
+#include <stdint.h>
+
+#include <unicore-mx/stm32/gpio.h>
+#include <unicore-mx/stm32/adc.h>
+#include <unicore-mx/stm32/rcc.h>
+
+struct AnalogMuxConfig {
+	uint32_t enPort;
+	uint16_t enPin;
+	uint32_t s0Port;
+	uint16_t s0Pin;
+	uint32_t s1Port;
+	uint16_t s1Pin;
+	uint32_t s2Port;
+	uint16_t s2Pin;
+	uint32_t s3Port;
+	uint16_t s3Pin;
+	uint32_t sigPort;
+	uint32_t sigPin;
+	uint16_t con;
+};
+
+
+//external
+void setupMux(struct AnalogMuxConfig config);
+
+void read(struct AnalogMuxConfig config, uint8_t retVals[]);
+
+void enable(struct AnalogMuxConfig config);
+
+void disable(struct AnalogMuxConfig config);
+
+
+
+//internal
+uint32_t whichClock(uint32_t bank);
+void writePin(uint32_t port, uint16_t pin, uint8_t value);
+
+
+
+
