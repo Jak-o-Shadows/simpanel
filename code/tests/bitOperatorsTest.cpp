95bad12e82cc41eb036e094cff3c79b14c559501
Jak_o_Shadows <simfake.mapping@gmail.com>
2018-09-03 21:34:42 +0930

    Working tests for the bitset type thing

--- code/tests/bitOperatorsTest.cpp
+++ code/tests/bitOperatorsTest.cpp
@@ -15,14 +15,48 @@ TEST(BitOperatorsTest, all)
 {
 	uint8_t halfword[] = {0x00, 0x00};
 	
-	setBit(byte, 1);
-	BITS_EQUAL(halfword[0], 0b1);
-	BITS_EQUAL(halfword[1], 0);
+	// Test setting bit
+	setBit(halfword, 1);
+	BYTES_EQUAL(0b10, halfword[0]);
+	BYTES_EQUAL(1, getBit(halfword, 1));
+	BYTES_EQUAL(0, halfword[1]);
 	
-	clearBit(byte, 1);
-	BITS_EQUAL(halfword[0], 0);
-	BITS_EQUAL(halfword[1], 0);
+	clearBit(halfword, 1);
+	BYTES_EQUAL(0, halfword[0]);
+	BYTES_EQUAL(0, getBit(halfword, 1));
+	BYTES_EQUAL(0, halfword[1]);
 	
-	FAIL();
+	// Test just before changing over byte
+	setBit(halfword, 7);
+	BYTES_EQUAL(0b10000000, halfword[0]);
+	BYTES_EQUAL(1, getBit(halfword, 7));
+	BYTES_EQUAL(0, halfword[1]);
+	
+	clearBit(halfword, 7);
+	BYTES_EQUAL(0, halfword[0]);
+	BYTES_EQUAL(0, getBit(halfword, 7));
+	BYTES_EQUAL(0, halfword[1]);
+	
+	// Test after changing over byte
+	setBit(halfword, 13);
+	BYTES_EQUAL(0, halfword[0]);
+	BYTES_EQUAL(1, getBit(halfword, 13));
+	BYTES_EQUAL(0b100000, halfword[1]);
+	
+	clearBit(halfword, 13);
+	BYTES_EQUAL(0, halfword[0]);
+	BYTES_EQUAL(0, getBit(halfword, 0));
+	BYTES_EQUAL(0, halfword[1]);
+	
+	// Test near the end of the second byte
+	setBit(halfword, 15);
+	BYTES_EQUAL(0, halfword[0]);
+	BYTES_EQUAL(1, getBit(halfword, 15));
+	BYTES_EQUAL(0b10000000, halfword[1]);
+	
+	clearBit(halfword, 15);
+	BYTES_EQUAL(0, halfword[0]);
+	BYTES_EQUAL(0, getBit(halfword, 15));
+	BYTES_EQUAL(0, halfword[1]);
 	
 } 
